67536a2aede1c014ba872cabfbb1291a2fd8d4df
Shahbaj Singh <ssingh@comqi.com>
2024-02-01 21:28:33 -0500

    Refactor for JSON parsing script

--- json_parsing.js
+++ json_parsing.js
@@ -1,23 +1,12 @@
-function tourl(out) {
-  console.log(out.requested_formats);
+function DataToUrl(out) {
+  //console.log(out.requested_formats);
   return out.requested_formats[1].url;
 }
-var audio_string = "Cannot get Audio Stream url for audio";
-const data = [
-  {
-    text: "Hello to the Other Side!",
-    url: "https://yt3.ggpht.com/ytc/AAUvwnjLXM7NgDeo3N4f5puZe5Thbv2QOlNgH6CMoHDhzw=s88-c-k-c0x00ffffff-no-rj",
-  },
-  {
-    text: "Rick Roll",
-    url: "https://yt3.ggpht.com/ytc/AAUvwni36SveDisR-vOAmmklBfJxnnjuRG3ihzfrwEfORA=s88-c-k-c0x00ffffff-no-rj",
-  },
-  {
-    text: "UwU vibes!",
-    url: "https://yt3.ggpht.com/ytc/AAUvwngMGZ-EFCcsO9w8BwxJbGFLv9TPBt6iamIIwRWa=s68-c-k-c0x00ffffff-no-rj",
-  },
-];
-function todata(SearchedData) {
+
+var audio_url= "https://livesim2.dashif.org/vod/testpic_2s/audio.mpd"; // Default link
+const data = [];
+
+function toData(SearchedData) {
   var data = [];
   for (var i in SearchedData) {
     data[i] = {};
@@ -27,4 +16,5 @@ function todata(SearchedData) {
   }
   return data;
 }
-module.exports = [tourl, data, todata, audio_string];
+
+module.exports = [DataToUrl, data, toData, audio_url];
